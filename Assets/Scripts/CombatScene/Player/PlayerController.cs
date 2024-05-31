ade486f30de8ff375c9a354cbfe63d79e9d66455
Park Sangyeol <tkddufdl77@ajou.ac.kr>
2024-05-31 20:42:25 +0900

    Feat: Player Movement
    -플레이어의 이동 구현

---
+++ Assets/Scripts/CombatScene/Player/PlayerController.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace  CombatScene.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [Title("Components")]
+        [InfoBox("components의 경우 따로 설정하지 않으면 GetComponent를 호출합니다.", InfoMessageType.Info)]
+        [SerializeField] 
+        private PlayerInput playerInput;
+        [SerializeField]
+        private Animator animator;
+        
+        private InputAction moveAction;
+        
+        void Awake()
+        {
+            // set Player Input
+            if (playerInput == null)
+            {
+                playerInput = GetComponent<PlayerInput>();
+            }
+
+            if (animator == null)
+            {
+                animator = GetComponent<Animator>();
+            }
+
+            moveAction = playerInput.actions["Move"];
+            moveAction.started += MovePlayer;
+            
+        }
+
+        private void OnEnable()
+        {
+            moveAction.Enable();
+        }
+
+        private void OnDisable()
+        {
+            moveAction.Disable();
+        }
+        
+        private void MovePlayer(InputAction.CallbackContext context)
+        {
+            // TODO 박제에 맞춰 눌렀는지 확인
+            // if ()
+            {
+                // 이동
+                Vector2 moveVal = context.ReadValue<Vector2>();
+                if (moveVal.x == 1 || moveVal.x == -1 || moveVal.y == 1 || moveVal.y == -1)
+                {
+                    if (Mathf.Abs(moveVal.x) == 1)
+                    {
+                        transform.localScale = new Vector3(-Mathf.Sign(moveVal.x), 1, 1);
+                    } 
+                    // 키보드를 하나만 입력. 실제 이동 구현
+                    StartCoroutine(MoveTo(moveVal, 0.5f));
+                }
+                else
+                {
+                    // 두 개의 키를 동시에 입력
+                    Debug.Log(GetType().Name + ": 두 개의 키를 동시에 눌렀습니다.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 특정한 타겟으로 플레이어를 일정시간동안 이동시키는 코루틴
+        /// </summary>
+        /// <param name="addPos"> 이동시킬 타겟 위치(현재 위치에서 더해주는 값)</param>
+        /// <param name="duration"> 이동 시간(초)</param>
+        /// <returns></returns>
+        IEnumerator MoveTo(Vector2 addPos, float duration)
+        {
+            // 플레이어 이동 애니메이션 재생
+            animator.SetTrigger("Move");
+            // 이동
+            Vector2 startPos = transform.position;
+            Vector2 targetPos = (Vector2)transform.position + addPos;
+            float time = 0;
+            while (duration > time)
+            {
+                transform.position = Vector2.Lerp(startPos, targetPos, time / duration);
+                time += Time.deltaTime;
+                yield return null;
+            }
+            transform.position = targetPos;
+        }
+        
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
